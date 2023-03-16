967a8de83f98ceec2b876c353fcd0c426f11a81a
royalcaster <gabriel.pechstein@schneeberg.km3.de>
2023-03-16 13:06:39 +0100

    HeroShot added, replaced gifs with static imgs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 
 //Style
 import './App.css';
+import HeroShot from './components/HeroShot/HeroShot';
 import Navbar from './components/Navbar/Navbar';
 
 //Service
@@ -17,21 +18,18 @@ const App = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-    console.log(language);
     return () => window.removeEventListener("scroll", handleScroll);
   });
 
   const handleScroll = () => {
     const currentYOffset = window.pageYOffset;
-    console.log(currentYOffset);
     const visible = yOffset > currentYOffset || yOffset < 150;
-
     setYOffset(currentYOffset);
     setVisible(visible);
   }
 
   const toggleLanguage = () => {
-    language.short == "de" ? setLanguage(languages.en) : setLanguage(languages.de);
+    language.short === "de" ? setLanguage(languages.en) : setLanguage(languages.de);
   }
 
   return (
@@ -41,31 +39,8 @@ const App = () => {
       <header>
           <Navbar show={visible} language={language} toggleLanguage={() => toggleLanguage()}/>
       </header>
-
-      <div style={{height: "100px"}}></div>
-       
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
-
-Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   
-
-Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.   
-
-Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.   
-
-Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.   
-
-At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.   
-
-Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus.   
-
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
-
-Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   
-
-Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.   
-
-Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
-
+    
+      <HeroShot language={language}/>
       
     </div>
   );
---
+++ src/components/HeroShot/HeroShot.js
@@ -0,0 +1,56 @@
+//React
+import { useEffect } from 'react';
+
+//Style
+import '../../App.css'
+
+const HeroShot = ({ language }) => {
+
+  //State
+  
+
+  useEffect(() => {
+   
+  },[]);
+
+  return (
+    <div className='content_container'>
+        <div className='heroshot_gif_container' style={{padding: "20px", height: "100vh", display: "flex", flexDirection: "row"}}>
+    
+        
+
+        <div style={{flex: 1, display: "flex", flexDirection: "column"}}>
+
+        <div style={{flex: 1}}></div>
+
+        <div style={{flex: 2, display: "flex", alignItems: "center", padding: "20px", textAlign: "center"}}>
+            {language.short === "de" ? 
+            <p className='heroshot_heading'>Das Werkzeug zum
+            <span style={{fontSize: "2.5rem", color: "#26FF72"}}> Erfassen,</span>
+            <span style={{fontSize: "2.5rem", color: "#FFBB24"}}> Auswerten </span>&
+            <span style={{fontSize: "2.5rem", color: "#FC2044"}}> Teilen </span>
+            deines Konsums</p>
+            : 
+            <p className='heroshot_heading'>The tool for 
+            <span style={{fontSize: "2.5rem", color: "#26FF72"}}> Tracking,</span>
+            <span style={{fontSize: "2.5rem", color: "#FFBB24"}}> Evaluating </span>& 
+            <span style={{fontSize: "2.5rem", color: "#FC2044"}}> Sharing </span> 
+            your consumption</p>}
+        </div>
+
+        <div style={{flex: 2, display: "flex", alignItems: "center"}}>
+            <button className='heroshot_download_button' title={language.navbar_get_the_app}>{language.navbar_get_the_app}</button>
+        </div>
+
+        </div>
+
+        <div className='heroshot_smartphone_container'>
+            .<img className='heroshot_smartphone_img' src={require("../../img/smartphone.png")} />
+        </div>
+
+        </div>
+    </div>
+  );
+}
+
+export default HeroShot;
