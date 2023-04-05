96eaa08f3a0a111020a96f4908fec519a9331719
royalcaster <gabriel.pechstein@schneeberg.km3.de>
2023-04-05 11:12:27 +0200

    Content Added

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -20,17 +20,36 @@ const Features = ({ language }) => {
 
             <div className="feature_panel_container">
 
-            <div style={{flex: 1}}>
+            <div style={{flex: 2, justifyContent: "center"}}>
                 {view === "main" ? <>
                     {language.features_main.map((text) => {
                         return <p className="feature_panel_text">{text}</p>
                     })}
                 </> : null}
-                {view === "stats" ? <p className="feature_panel_text">{language.features_stats}</p> : null}
+                {view === "stats" ? <>
+                    {language.features_stats.map((text) => {
+                        return <p className="feature_panel_text">{text}</p>
+                    })}
+                </> : null}
+                {view === "map" ? <>
+                    {language.features_map.map((text) => {
+                        return <p className="feature_panel_text">{text}</p>
+                    })}
+                </> : null}
+                {view === "config" ? <>
+                    {language.features_config.map((text) => {
+                        return <p className="feature_panel_text">{text}</p>
+                    })}
+                </> : null}
+                {view === "friends" ? <>
+                    {language.features_friends.map((text) => {
+                        return <p className="feature_panel_text">{text}</p>
+                    })}
+                </> : null}
             </div>
 
             <div style={{flex: 1}}>
-                <img src={require("./../../img/smartphone.png")} style={{maxWidth: "300px", marginLeft: "auto", marginRight: "auto"}}/>
+                <img src={require("./../../img/smartphone.png")} style={{maxWidth: "300px", marginLeft: "auto", marginRight: "auto", display: "block"}}/>
             </div>
 
             </div>
